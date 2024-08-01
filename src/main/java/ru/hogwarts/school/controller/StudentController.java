dfe3a5dc8aaa8cfd99bb6027f926ea2ea34836b1
darya <darya.kolycheva98@yandex.ru>
2024-08-01 18:31:13 +0300

    HW 3.2 готово

---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,43 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public Student create(@RequestBody Student student) {
+        return studentService.create(student);
+    }
+
+    @GetMapping("{id}")
+    public Student read(@RequestParam Long id) {
+        return studentService.read(id);
+    }
+
+    @PutMapping("{id}")
+    public Student update(@PathVariable Long id, @RequestBody Student student) {
+        return studentService.update(id, student);
+    }
+
+    @DeleteMapping("{id}")
+    public Student delete(@PathVariable Long id) {
+        return studentService.delete(id);
+    }
+
+    @GetMapping("/filter/{age}")
+    public List<Student> filterByAge(@RequestParam int age) {
+        return studentService.filterByAge(age);
+    }
+
+}
