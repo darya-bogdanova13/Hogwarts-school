fc0be67340fb30c2f52c6539dd4e4d6dbdb8838f
darya <darya.kolycheva98@yandex.ru>
2024-10-06 14:02:17 +0300

    HW 4.5 готово

--- src/main/java/ru/hogwarts/school/SchoolApplication.java
+++ src/main/java/ru/hogwarts/school/SchoolApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition
 
 public class SchoolApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
